fdeb52c32fe0caab5953b1776016db2de1c46794
Raviteja Gunty <ravitejagunty@gmail.com>
2017-06-22 01:27:12 -0500

    Add files via upload

---
+++ tone_analyzer.v3.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+const tone_analyzer = new ToneAnalyzerV3({
+  username: 'INSERT YOUR USERNAME FOR THE SERVICE HERE',
+  password: 'INSERT YOUR PASSWORD FOR THE SERVICE HERE',
+  version_date: '2016-05-19'
+});
+
+tone_analyzer.tone({ text: 'Greetings from Watson Developer Cloud!' }, function(err, tone) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('tone endpoint:');
+    console.log(JSON.stringify(tone, null, 2));
+  }
+});
+
+const params = {
+  utterances: [
+    { text: 'My charger isn’t working.', user: 'customer' },
+    { text: 'Thanks for reaching out. Can you give me some more detail about the issue?', user: 'agent' },
+    {
+      text: "I put my charger in my phone last night to charge and it isn't working. Which is ridiculous, it's a new charger, I bought it yesterday.",
+      user: 'customer'
+    },
+    { text: 'I’m sorry you’re having issues with charging. What kind of charger do you have?', user: 'agent' }
+  ]
+};
+
+tone_analyzer.tone_chat(params, function(err, tone) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('tone_chat endpoint:');
+    console.log(JSON.stringify(tone, null, 2));
+  }
+});
